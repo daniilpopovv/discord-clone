c57be048e1ec34d1816af72bf0fc190654e8859c
daniilpopovv <warcatyt@gmail.com>
2023-10-16 22:08:01 +0300

    themes-setup/wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import React from 'react';
 
+import { ThemeProvider } from '@/components/providers/theme-provider';
+import { cn } from '@/lib/utils';
+
 const font = Open_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,8 +22,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, 'bg-white dark:bg-[#313338]')}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
