3d3299079badd678f164fe571e1c09acdfc699fa
daniilpopovv <warcatyt@gmail.com>
2023-10-28 22:31:48 +0300

    server-channels-list/wip

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,4 +1,4 @@
-import { Server } from '@prisma/client';
+import { ChannelType, Server } from '@prisma/client';
 import { create } from 'zustand';
 
 type ServerModalType = 'createServer' | 'editServer' | 'deleteServer';
@@ -9,6 +9,7 @@ export type ModalType = ServerModalType | ChannelModalType | MemberModalType;
 
 interface ModalData {
   server?: Server;
+  channelType?: ChannelType;
 }
 
 interface ModalStore {
